19121bf1dee2371a66c7314522712b37f7ee8a67
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2025-09-28 13:01:49 +0530

    add separate config for dev and prod envs - split the docker compose into separate cofiguration files for development and production

--- node-js-ts/server.ts
+++ node-js-ts/server.ts
@@ -10,6 +10,16 @@ const PORT = process.env.PORT;
 
 const server = http.createServer();
 
+// server.on("request", (req, res) => {
+// 	console.log("request recieved", req);
+// 	res.write("request written \n");
+// 	res.end("end of response");
+// });
+
+// server.on("error", (err) => {
+// 	console.log("error", err);
+// });
+
 server.listen(PORT, () => {
 	console.log(`⚙️ Server listening on PORT :${PORT}!!`);
 });
